10dbca1288faac012dc8a9ba1ec678f03ddfd574
TheEmper <alexmikeladze1@gmail.com>
2024-02-28 18:02:30 +0400

    first commit

---
+++ day 7/classwork/classwork.py
@@ -0,0 +1,15 @@
+# ცვლადის წარმოდგენა ჯამის შესანახად
+total = 0
+
+# გამეორება რიცხვების 1-დან 10-მდე
+for num in range(1, 11):
+    # მივუმატოთ მიმიდნარე რიცხვები ჯამს
+    total += num
+
+# output
+print("The sum of numbers from 1 to 10 is:", total)
+
+
+# დავალება 1
+for i in range(1, 11):
+    print(i)
---
+++ day 7/homework/day7.py
@@ -0,0 +1,21 @@
+
+
+# N1
+for num in range(2, 11, 2):
+    print(num)
+
+# N2
+num = 1
+while num <= 20:
+    print(num)
+    num += 1
+
+# N3
+sum_numbers = 0
+num = 0
+while num <= 5:
+    sum_numbers += num
+    num += 1
+print("The sum of numbers from zero to five is:", sum_numbers)
+
+# N4
