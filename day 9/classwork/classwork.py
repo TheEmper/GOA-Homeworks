732de9fcaee39767e081d28a19e25be6f814930a
TheEmper <alexmikeladze1@gmail.com>
2024-03-03 23:04:38 +0400

    third commit

---
+++ day 9/classwork/classwork.py
@@ -0,0 +1,7 @@
+# num = int(input("Give me a number: "))
+
+# print(num > 1 and num < 10)
+
+num = int(input("Give me a number: "))
+
+print(num % 5 == 0 and num % 10 == 0)
---
+++ day 9/homework/homework.py
@@ -0,0 +1,17 @@
+num = int(input("Give me a number: "))
+
+print(num % 2 == 0 and num % 4 == 0)
+
+# შედარების ოპერატორები / and operators
+
+print(5 > 10 and 6 < 3)
+print(6 < 3 and 5 < 9)
+print(9 > 2 and 1 < 3)
+print(5 == 3 and 3 < 3)
+print(4 != 3 and 2 > 1)
+print(3 != 6 and 4 != 2)
+print(6 >= 6 and 3 == 3)
+print(7 <= 9 and 9 >= 9)
+print(11 == 11 and 22 >= 1)
+print(15 != 15 and 2 < 100)
+
