3762d2e968caa97b983822184687ea7cf11d1c09
TheEmper <alexmikeladze1@gmail.com>
2024-03-10 13:00:41 +0400

    fourth

---
+++ day 10/classwork/classwork.py
@@ -0,0 +1,31 @@
+# age = int(input("What's your age?: "))
+
+# if age >= 0 and age < 11:
+#   print("You are a kid")
+# elif age >= 11 and age < 18:
+#   print("You are a teen")
+# elif age >= 18 and age < 60:
+#   print("You are an adult")
+# elif age < 0:
+#   print("Error, try again.")
+# else:
+#   print("You are old")
+
+
+# davaleba 1
+favorite_movies = ["Inception", "The Shawshank Redemption", "The Godfather", "The Dark Knight", "Pulp Fiction"]
+print("Favorite Movies List:")
+print(favorite_movies)
+print("One movie from the list:")
+print(favorite_movies[0])
+
+# davaleba 2
+integer_list = [14, 25, 7, 36, 42, 19, 8, 53]
+print("List of Integers:", integer_list)
+largest_number = max(integer_list)
+print("Largest Number in the List:", largest_number)
+
+# davaleba 3
+smallest_numbers = sorted(integer_list)[:2]
+print("Two Smallest Numbers from the List:", smallest_numbers)
+
---
+++ day 10/homework/homework.py
@@ -0,0 +1,31 @@
+# task 1
+
+programming_languages = ["Python", "HTML", "CSS", "C++", "C#"]
+
+print(programming_languages)
+print(programming_languages[-1])
+
+# task 2
+
+data_types = ["String", 2, 2.2, True]
+
+print(data_types[0])
+print(data_types[1])
+print(data_types[2])
+print(data_types[3])
+
+# task 3
+
+numbers = [4, 8, 12, 16, 20]
+
+print("Largest number:",max(numbers))
+
+# task 4
+
+nums = [31, 33, 35, 37, 39, 41, 43, 45, 47, 49]
+
+print(nums[0] + nums[1] + nums[2])
+
+# task 5
+
+print(nums[1], nums[4], nums[7])
