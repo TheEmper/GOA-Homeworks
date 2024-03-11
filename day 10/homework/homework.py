50304791acd0ed0b314dd1c3fc7b67a465ba7d50
TheEmper <alexmikeladze1@gmail.com>
2024-03-11 20:10:05 +0400

    fourth commit

--- day 10/homework/homework.py
+++ day 10/homework/homework.py
@@ -1,11 +1,11 @@
-# task 1
+# task 1 შექმენით სია პროგრამირების ენების შესახებ, შეიტანეთ 5 პროგრამირების ენა. დაპრინტეთ  ჯერ მთლიანი სია, შემდეგ კი ბოლო ელემენტი.
 
 programming_languages = ["Python", "HTML", "CSS", "C++", "C#"]
 
 print(programming_languages)
 print(programming_languages[-1])
 
-# task 2
+# task 2 შექმენით სია, სადაც გექნებათ შეტანილი string, integer, float, boolean. საბოლოოდ ყველა ელემენტი ცალ-ცალკე გამოიტანეთ.
 
 data_types = ["String", 2, 2.2, True]
 
@@ -14,18 +14,19 @@ print(data_types[1])
 print(data_types[2])
 print(data_types[3])
 
-# task 3
+# task 3 შექმენით სია, სადაც გექნებათ 0-იდან 20-ის ჩათვლით 4-ის ჯერადები და პრინტით გამოიტანეთ უდიდესი.
+
 
 numbers = [4, 8, 12, 16, 20]
 
 print("Largest number:",max(numbers))
 
-# task 4
+# task 4 შექმენით სია სადაც შეტანილი გექნებათ 30-იდან 50-ის ჩათვლით კენტი რიცხვები. შემდეგ დაპტინტეთ სამი უმცირესი ელემენტის ჯამი.
 
 nums = [31, 33, 35, 37, 39, 41, 43, 45, 47, 49]
 
 print(nums[0] + nums[1] + nums[2])
 
-# task 5
+# task 5 დაპრინტეთ სამის ჯერადები დავალება 4-ის სიიდან.
 
 print(nums[1], nums[4], nums[7])
