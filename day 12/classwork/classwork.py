3fc32e88bcbe6181d759f06eb92f3212160d0205
TheEmper <alexmikeladze1@gmail.com>
2024-03-16 13:54:55 +0400

    millionth commit

---
+++ day 12/classwork/classwork.py
@@ -0,0 +1,43 @@
+# Basic List Slicing Syntax:
+
+# list_name[start:end:step]
+
+# list_name: The name of the list you want to slice.
+# start: The index of the first element you want to include in the slice.
+# end: The index of the element just after the last element you want to include in the slice.
+# step: The increment between indices. (Optional, defaults to 1)
+
+#Examples:
+
+# my_list = [1, 2, 3, 4, 5]
+
+# Accessing a single element
+# print(my_list[0])  # Output: 1
+
+# # Accessing a range of elements
+# print(my_list[1:4])  # Output: [2, 3, 4]
+
+# my_list = [1, 2, 3, 4, 5]
+
+# # Omitting start index (start from the beginning)
+# print(my_list[:3])  # Output: [1, 2, 3]
+
+# # Omitting end index (go till the end)
+# print(my_list[2:])  # Output: [3, 4, 5]
+
+# my_list = [1, 2, 3, 4, 5]
+
+# # Negative indices count from the end of the list
+# print(my_list[-3:])  # Output: [3, 4, 5]
+
+# # Negative step reverses the list
+# print(my_list[::-1])  # Output: [5, 4, 3, 2, 1]
+
+# my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# # Using step to get every other element
+# print(my_list[::2])  # Output: [1, 3, 5, 7, 9]
+
+# # Reversing with a step
+# print(my_list[::-1])  # Output: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
---
+++ day 12/homework/homework.py
@@ -0,0 +1,63 @@
+# # davaleba 1
+user_info = []
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+age = int(input("Enter your age: "))
+residence = input("Enter your place of residence: ")
+
+user_info.append(first_name)
+user_info.append(last_name)
+user_info.append(age)
+user_info.append(residence)
+
+print("1:", user_info[0], user_info[1])
+print("2:", user_info[1], user_info[2])
+print("3:", user_info[0], user_info[1], user_info[2])
+print("4:", user_info[1], user_info[2], user_info[3])
+
+# davaleba 2
+
+nums = []
+
+negative_num = int(input("Enter a negative number: "))
+
+for i in range(negative_num, 0):
+  nums.append(i)
+
+# print(nums)
+
+# # davaleba 3
+
+full_name = "Sandro Miqeladze"
+first_name = full_name[:6]
+last_name = full_name[-9:]
+
+print(first_name)
+print(last_name)
+
+# davaleba 4
+
+movies = ["Whispers of the Forgotten", "Neon Dreamscape", "Eternal Echoes", "Midnight Mirage", "Rogue Constellation"]
+
+print(movies[1:3])
+print(movies[:3])
+print(movies[2:])
+
+#neg sliciing
+
+print(movies[-4:-2])
+print(movies[-5:-2])
+print(movies[-3:])
+
+# davaleba 5
+
+academy = input("What academy do you go to?: ")
+
+if academy[0] == "G":
+  print("GOA is the correct choice.")
+else:
+  print("Wrong choice.")
+
+
+
